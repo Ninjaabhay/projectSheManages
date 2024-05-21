5c987ba502030cb43bf7f25d61566b3c86c8ed5b
Ninjaabhay <93691633+Ninjaabhay@users.noreply.github.com>
2024-05-22 01:34:55 +0530

    Update IdeaPage.jsx

--- frontend/src/components/IdeaPage.jsx
+++ frontend/src/components/IdeaPage.jsx
@@ -34,7 +34,7 @@ const IdeaPage = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/ideas/submit",
+        "https://shemanages.vercel.app/api/ideas/submit",
         {
           name: form.name,
           email: form.email,
