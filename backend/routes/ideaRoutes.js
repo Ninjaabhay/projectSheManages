be76b62f0d5cfd9916eca7c6c8cd1edb609e09d7
Abhay Tiwari <93691633+Ninjaabhay@users.noreply.github.com>
2024-05-28 12:15:17 +0530

    Delete backend directory

--- backend/routes/ideaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { submitIdea, getIdeas } = require("../controllers/ideaController");
-const router = express.Router();
-
-router.post("/submit", submitIdea);
-router.get("/", getIdeas);
-
-module.exports = router;
