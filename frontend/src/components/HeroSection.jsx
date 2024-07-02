64d5f6651ea6fa38f1c4c05fd6178f45e2b0a8e6
Abhay Tiwari <93691633+Ninjaabhay@users.noreply.github.com>
2024-07-02 22:45:01 +0530

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import HeroImage from "../images/Hero Image.gif";
 import HeroWave from "../images/wave (10).svg";
 import { useAuth } from "../context/AuthProvider";
